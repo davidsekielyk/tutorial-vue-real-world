0e42ee9ca6bd060bf435a3cc71d5e04e02ce7743
davidsekielyk <david.sekielyk@gmail.com>
2019-04-19 01:07:58 -0300

    dynamic routing and history mode

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>Event Listing</h1>
+    <div>
+        <h1>Event Listing</h1>
+        <p>
+            <router-link :to="{ name: 'event-show', params: { id: '1' } }">Firs Event</router-link>
+        </p>
+    </div>
 </template>
